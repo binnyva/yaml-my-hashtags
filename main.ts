e9b9f76f834ba1be6b2436541085655c4d293ccc
Binny V A <binnyva@gmail.com>
2021-07-17 03:24:53 +0530

    First Commit

--- main.ts
+++ main.ts
@@ -1,39 +1,89 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as matter from 'gray-matter';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface YamlMyHashtagsSettings {
+	singleLineTags: boolean;
+	removeHashTags: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: YamlMyHashtagsSettings = {
+	singleLineTags: true,
+	removeHashTags: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const uniqueArray = function(arr: any[]): any[] {
+    for(var i=0; i<arr.length; ++i) {
+        for(var j=i+1; j<arr.length; ++j) {
+            if(arr[i] === arr[j])
+                arr.splice(j--, 1);
+        }
+    }
+
+    return arr;
+}
+
+export default class YamlMyHashtags extends Plugin {
+	settings: YamlMyHashtagsSettings;
 
 	async onload() {
-		console.log('loading plugin');
-
-		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'convert-tags-to-yaml',
+			name: 'Convert Hashtags to YAML Frontmatter',
+			callback: async () => {
+				await this.loadSettings();
+
+				const noteFile = this.app.workspace.getActiveFile()
+				const metadata = this.app.metadataCache.getFileCache(noteFile)
+				
+				if (noteFile.name) {
+					// let leaf = this.app.workspace.activeLeaf;
+					// const container = leaf.view?.containerEl;
+					// let text = container.innerText; // getText() does not work
+					let text = await this.app.vault.read(noteFile);
+
+					let hashTagRegexp = /#([^\s#,]+)/g;
+					const tagMatches = [...text.matchAll(hashTagRegexp)]
+					const tags = tagMatches.map( ele => ele[1])
+
+					if(!tags.length) return false; // No tags found
+
+					const note = matter.parse(text) // Is function was added to the library by me.
+
+					let newNote = text
+
+					// For more options, go to https://github.com/nodeca/js-yaml#dump-object---options-
+					const yamlOptions = {
+						flowLevel: (this.settings.singleLineTags) ? 1 : -1, 
+						forceQuotes: true, // if true this will quote everything
+						// quotingType: '"',
+						// styles: {
+						// 	'!!timestamp': 'YYYY-MM-DD' // Does not work
+						// },
 					}
+					if(matter.test(text)) {
+						let frontmatter = note.data // We can use metadata.frontmatter too(?)
+						if(frontmatter.tags) {
+							frontmatter.tags = uniqueArray(frontmatter.tags.concat(tags))
+						} else {
+							frontmatter.tags = uniqueArray(tags)
+						}
+
+						newNote = matter.stringify(note.content, frontmatter, yamlOptions)
+
+					} else {
+						newNote = matter.stringify(text, {tags: uniqueArray(tags)}, yamlOptions)
+					}
+
+					// If setting is enabled, remove the hashtags.
+					if(this.settings.removeHashTags) {
+						for(let i in tags) {
+							newNote = newNote.replace(`#${tags[i]}`, "")
+						}
+					}
+
+					// Write the updated version to the file
+					await this.app.vault.modify(noteFile, newNote);
+					
 					return true;
 				}
 				return false;
@@ -41,20 +91,6 @@ export default class MyPlugin extends Plugin {
 		});
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
 	}
 
 	async loadSettings() {
@@ -66,26 +102,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: YamlMyHashtags;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: YamlMyHashtags) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -94,18 +114,25 @@ class SampleSettingTab extends PluginSettingTab {
 		let {containerEl} = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Yaml My Tags'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+			.setName('Single Line Tag List')
+			.setDesc('Do you want single line tags(tags: [tag-1,tag-2,tag-3]) or multi line tags?')
+			.addToggle(text => text
+				.setValue(true)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.singleLineTags = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Remove Hashtags after Converting')
+			.setDesc('Remove hashtags from the text after moving them to the YAML frontmatter area?')
+			.addToggle(text => text
+				.setValue(true)
+				.onChange(async (value) => {
+					this.plugin.settings.removeHashTags = value;
 					await this.plugin.saveSettings();
 				}));
 	}
